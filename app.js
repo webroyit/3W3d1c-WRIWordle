ef6e71c44de35290387ecfaf218e1ac917aa6064
royweb <xytank@gmail.com>
2022-04-04 13:20:40 -0400

    Create grid and tiles with styles

--- app.js
+++ app.js
@@ -32,9 +32,37 @@ const keys = [
     '«',    // Backspace
 ]
 
+const guessRows = [
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', '']
+]
+
+// Create grid
+guessRows.forEach((guessRow, guessRowIndex) => {
+    const rowElement = document.createElement('div')
+    rowElement.setAttribute('id', 'guessRow-' +guessRowIndex)
+    guessRow.forEach((guess, guessIndex) => {
+        const tileElement = document.createElement('div')
+        tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex)
+        tileElement.classList.add('tile')
+        rowElement.append(tileElement)
+    })
+    tileDisplay.append(rowElement)
+})
+
+const handleClick = () => {
+    console.log('clicked')
+}
+
+// Create Keyboard keys
 keys.forEach(key => {
     const buttonElement = document.createElement('button')      // Create Button for eact keys
     buttonElement.textContent = key                             // Set the text of the button
     buttonElement.setAttribute('id', key)                       // Set the ID name for button
+    buttonElement.addEventListener('click', handleClick)        // Add event listener to the button
     keyboard.append(buttonElement)                              // Add New buttons to the DOM
 })
