67c9d59aca42d9d3cfbf5d02ff5553c41173b5f9
royweb <xytank@gmail.com>
2022-04-11 12:43:35 -0400

    Add animation

--- app.js
+++ app.js
@@ -138,13 +138,17 @@ const flipTile = () => {
     rowTiles.forEach((tile, index) => {
         const dataLetter = tile.getAttribute('data')
 
-        if (dataLetter == wordle[index]) {
-            tile.classList.add('green-overlay')
-        } else if (wordle.includes(dataLetter)) {
-            tile.classList.add('yellow-overlay')
-        } else {
-            tile.classList.add('grey-overlay')
-        }
+        setTimeout(() => {
+            tile.classList.add('flip')
+            
+            if (dataLetter == wordle[index]) {
+                tile.classList.add('green-overlay')
+            } else if (wordle.includes(dataLetter)) {
+                tile.classList.add('yellow-overlay')
+            } else {
+                tile.classList.add('grey-overlay')
+            }
+        }, 500 * index)     // Increment each one by index so that they do not flip at the same time
     })
 }
 
